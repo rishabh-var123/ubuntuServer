ed84ba4ea208877d8dd5761b4f43dcb42b39cabe
rishabh <rishabhanmol6@gmail.com>
2023-10-12 12:05:19 +0530

    userprojectnew

--- user_project.sh
+++ user_project.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 
 echo "Welcome to the User Management Project created by - Rishabh Varshney"
+#to create a user
+function create_user{
 
+}
+
+#to delete a user
+function delete_user{
+}
+
+#to reset password of a user
+function reset_user{
+}
+
+#to list a user
+function list_user{
+}
+
+#to display user option
+function display_usage{
+	echo "Usage : $0 [Options]"
+	echo "[Option]: -"
+	echo "-c or --create  Create a new user account"
+        echo "-d or --delete  Delete a user account"
+        echo "-r or --reset   Reset the password for user account"
+        echo "-l or --list    List all the  user account"
+        echo "-h or --help  Display the help and exit"
+}
+
+#check if no argument provide or --help option
+if [ $# -eq 0 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+	display_usage
+fi
+
+#for command line arguents
 while [ $# -gt 0 ];do
 
 	case "$1" in
@@ -29,4 +62,4 @@ done
 
 
 
-done
+
