ffdb7325fda1d9bc818c8239d80219502c0eeb18
RishabhVarshney <rishabhvarshney0309@gmail.com>
2023-10-14 11:42:49 +0530

    add user2

--- user_project.sh
+++ user_project.sh
@@ -2,32 +2,48 @@
 
 echo "Welcome to the User Management Project created by - Rishabh Varshney"
 #to create a user
-function create_user{
-
+function create_user {
 read -p "Enter the user name :- " user
-if  id "$user" &>/dev/null ; then
-	echo "Error: The username "$user" is already present.Please enter the differnt user."
-else
-       read -p "Enter the password for user:- " password
-	useradd -m -p "$password" "$user"
-	echo "Useradd Successfully"
-fi
+	if id "$user" &>/dev/null;then
+		echo "Error: The username "$user" is already present.Please enter the differnt user."
+	else
+       		read -p "Enter the password for user:- " password
+		useradd -m -p "$password" "$user"
+		echo "Useradd Successfully"
+	fi
 }
 
 #to delete a user
-function delete_user{
+function delete_user {
+read -p "Enter the user for delete :" username
+if id "$username" &>/dev/null;then
+	userdel -r "$username"
+	echo "Username "$username" is deleted successfully"
+else
+	echo "Error: Username "$username" is not present"
+fi
 }
 
 #to reset password of a user
-function reset_user{
+function reset_user {
+read -p "Enter the user for reset the password :" username
+if id "$username" &>/dev/null;then
+	read -p "Enter the new password for user" pass
+	echo "$username:$pass" | chpasswd
+        echo "Username "$username" password is change successfully"
+else
+        echo "Error: Username "$username" is not present"
+fi
 }
 
 #to list a user
-function list_user{
+function list_user {
+echo "User accounts on the system "
+cut -d: f1 /etc/passwd 
 }
 
 #to display user option
-function display_usage{
+function display_usage {
 	echo "Usage : $0 [Options]"
 	echo "[Option]: -"
 	echo "-c or --create  Create a new user account"
